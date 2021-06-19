2f5b2911ecdc53826813610a59ff4df682b13fb6
Hoosier-Daddy <uttkarshbansal13@gmail.com>
2021-06-19 21:10:59 +0530

    fix feature
    removed radio buttons, instead using select button

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ListOfProducts from './ListOfProducts';
+
 
 const Filter = () => {
 
@@ -9,6 +10,7 @@ const Filter = () => {
     const [size, setSize] = useState(null);
     const [brand, setBrand] = useState(null);
     const [idealFor, setidealFor] = useState(null);
+    // const inputRef = useRef();
 
 
 
@@ -132,33 +134,34 @@ const Filter = () => {
         <>
             <header>
                 <form onSubmit={handleSubmit}>
-                    <div>
-                        <span className="filter-name">Size</span>
-                        <input type="radio" id="S" name="size" value="S" onChange={handleSize} />
-                        <label for="S">S</label>
-                        <input type="radio" id="M" name="size" value="M" onChange={handleSize} />
-                        <label for="M">M</label>
-                        <input type="radio" id="L" name="size" value="L" onChange={handleSize} />
-                        <label for="L">L</label>
-                        <input type="radio" id="XL" name="size" value="XL" onChange={handleSize} />
-                        <label for="XL">XL</label>
-                    </div>
-                    <div>
-                        <span className="filter-name">Brand</span>
-                        <input type="radio" id="adidas" name="brand" value="Adidas" onChange={handleBrand} />
-                        <label for="adidas">Adidas</label>
-                        <input type="radio" id="puma" name="brand" value="Puma" onChange={handleBrand} />
-                        <label for="puma">Puma</label>
-                        <input type="radio" id="nike" name="brand" value="Nike" onChange={handleBrand} />
-                        <label for="nike">Nike</label>
-                    </div>
-                    <div>
-                        <span className="filter-name">Ideal For</span>
-                        <input type="radio" id="men" name="idealFor" value="Men" onChange={handleIdealFor} />
-                        <label for="men">Men</label>
-                        <input type="radio" id="women" name="idealFor" value="Women" onChange={handleIdealFor} />
-                        <label for="women">Women</label>
-                    </div>
+
+                    <label for="size-filter">Choose a size:</label>
+
+                    <select value={size} id="size-filter" onChange={handleSize}>
+                        <option value="">--Please choose an option--</option>
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                    </select>
+
+                    <label for="brand-filter">Choose a Brand:</label>
+
+                    <select value={brand} id="brand-filter" onChange={handleBrand}>
+                        <option value="">--Please choose an option--</option>
+                        <option value="Adidas">Adidas</option>
+                        <option value="Puma">Puma</option>
+                        <option value="Nike">Nike</option>
+                    </select>
+                    <label for="idealFor-filter">Ideal for:</label>
+
+                    <select value={idealFor} id="idealFor" onChange={handleIdealFor}>
+                        <option value="">--Please choose an option--</option>
+                        <option value="Men">Men</option>
+                        <option value="Women">Women</option>
+
+                    </select>
+
                     <div>
                         <button type="submit" onClick={clearFilter}>Clear all Filters</button>
                         <button type="submit">Filter</button>
